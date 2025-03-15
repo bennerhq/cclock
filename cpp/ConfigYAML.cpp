34582eb1ec1b39527f31317fddce7b62458be7be
Jens Kaas Benner <jens@bennerhq.com>
2025-03-15 09:35:22 +0100

    Refactor color configuration in cclock.yaml and move default settings to ConfigDefault.h

--- cpp/ConfigYAML.cpp
+++ cpp/ConfigYAML.cpp
@@ -17,34 +17,9 @@
 
 YAML::Node config;
 
-YAML::Node default_config = YAML::Load(R"(
-    colors:
-        background: '#00FFFFFF'
-        dial: '#00FFFFFF'
-        dial_frame: "black"
-
-        date_background: '#000000'
-        date: '#FFFFFF'
-        date_font: 'Courier New'
-
-        hour_mark: '#000000'
-        minute_mark: '#000000'
-
-        second_hand: '#FF0000'
-        hour_hand: '#000000'
-        minute_hand: '#000000'
-        middle_dot: '#000000'
-
-    window:
-        animate: "float"
-        frameless: true
-        always_on_top: true
-        tool: true
-        x: 20
-        y: 20
-        width: 200
-        height: 200
-)");
+YAML::Node default_config = YAML::Load(
+    #include "h/ConfigDefault.h"
+);
 
 YAML::Node config_merge(const YAML::Node& default_config, const YAML::Node& config) {
     YAML::Node merged_config = default_config;
---
+++ h/ConfigDefault.h
@@ -0,0 +1,28 @@
+R"(
+    colors:
+        background: '#00FFFFFF'
+        dial: '#00FFFFFF'
+        dial_frame: "black"
+
+        date_background: '#000000'
+        date: '#FFFFFF'
+        date_font: 'Courier New'
+
+        hour_mark: '#000000'
+        minute_mark: '#000000'
+
+        second_hand: '#FF0000'
+        hour_hand: '#000000'
+        minute_hand: '#000000'
+        middle_dot: '#000000'
+
+    window:
+        animate: "float"
+        frameless: true
+        always_on_top: true
+        tool: true
+        x: 20
+        y: 20
+        width: 200
+        height: 200
+)"
