886cc2e569cb5d061a505a9c87e511d7ced93678
Jens Kaas Benner <jens@bennerhq.com>
2025-03-17 07:15:35 +0100

    Update ClockWidget.cpp

--- cpp/ClockWidget.cpp
+++ cpp/ClockWidget.cpp
@@ -107,7 +107,7 @@ void ClockWidget::paintEvent(QPaintEvent*) {
     // Draw day number at 15:00 o'clock
     if (date_background_color.isValid()) {
         int hour_marker = config["colors"]["date_position"].as<int>();
-        int angle = hour_marker * 30 - 30*3; // ?
+        int angle = 30*(hour_marker - 30); // ?
         int x = 90 * std::cos(qDegreesToRadians(static_cast<double>(angle)));
         int y = 90 * std::sin(qDegreesToRadians(static_cast<double>(angle)));
         QRect rect(x - 10, y - 10, 20, 20);
